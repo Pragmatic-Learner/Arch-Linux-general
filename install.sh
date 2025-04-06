7d599917afddce470c7d957031cf116f860342f0
Eternal Learner <nardevjuggoostudying@gmail.com>
2025-04-06 19:47:26 +0400

    7th commit

--- install.sh
+++ install.sh
@@ -1,40 +1,42 @@
 #!/bin/bash
+#setting up time
 timedatectl set-ntp true
-echo -e "g\nn\n\n\n+512\nn\n\n\n+14G\nn\n\n\n\nt\n1\n1\nt\n2\n19\nw\n" | fdisk /dev/sda
 
+#setting up partition and filesystems
+echo -e "g\nn\n\n\n+512MiB\nn\n\n\n+4G\nn\n\n\n\nt\n1\n1\nt\n2\n19\nw\n" | fdisk /dev/sda
 mkfs.fat -F 32 /dev/sda1
-mkfs.ext4 /dev/sda3
 mkswap /dev/sda2
+mkfs.ext4 /dev/sda3
 
+#mounting partitions
 mount /dev/sda3 /mnt
 mount --mkdir /dev/sda1 /mnt/boot
 swapon /dev/sda2
 
-pacstrap -K /mnt base linux linux-firmware git neovim
-
+#installing packages
+pacstrap -K /mnt base base-devel linux linux-firmware git neovim efibootmgr networkmanager pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber reflector openssh man sudo
+#generate automatic mount points
 genfstab -U /mnt >> mnt/etc/fstab
 
-nvim /mnt/install.sh >ln -sf /usr/share/zoneinfo/Time/Zone /etc/localtine
-nvim /mnt/install.sh >> hwclock --systohc
-nvim /mnt/install.sh >> nvim /etc/locale.gen > "en_US.UTF-8 UTF-8"
-nvim /mnt/install.sh >> nvim /etc/locale.gen >> "en_GB.UTF-8 UTF-8"
-nvim /mnt/install.sh >> locale-gen
-nvim /etc/locale.conf > "LANG=en\_GB.UTF-8 UTF-8"
-nvim /mnt/install.sh >> nvim /etc/vconsole > "KEYMAP=us"
-nvim /mnt/install.sh >> nvim /etc/hostname > "ARCH"
-nvim /mnt/install.sh >> nvim /etc/hosts > "123.0.0.1 localhost"
-nvim /mnt/install.sh >> nvim /etc/hosts > "::1 localhost"
-nvim /mnt/install.sh >> nvim /etc/hosts > "123.0.1.1 ARCH"
-nvim /mnt/install.sh >> passwd "1234"
-nvim /mnt/install.sh >> useradd -mG wheel "user1"
-nvim /mnt/install.sh >> passwd "1234"
-nvim /mnt/install.sh >> grub-install --target=x86\_64-efi --efi-directory=/boot --bootloader-id=GRUB
-nvim /mnt/install.sh >> grub-mkconfig -o /boot/grub/grub.cfg
+cd /mnt
+echo "ln -sf /usr/share/zoneinfo/Time/Zone /etc/localtine" >> /mnt/install.sh
+echo "hwclock --systohc" >> /mnt/install.sh
+echo "nvim /etc/locale.gen > "en_US.UTF-8 UTF-8"" >> /mnt/install.sh
+echo "nvim /etc/locale.gen >> "en_GB.UTF-8 UTF-8"" >> /mnt/install.sh
+echo "locale-gen" >> /mnt/install.sh
+echo "/etc/locale.conf > "LANG=en_GB.UTF-8 UTF-8"" >> /mnt/install.sh
+echo "nvim /etc/vconsole > "KEYMAP=us"" >> /mnt/install.sh
+echo "nvim /etc/hostname > "ARCH"" >> /mnt/install.sh
+echo "nvim /etc/hosts > "123.0.0.1 localhost"" >> /mnt/install.sh
+echo "nvim /etc/hosts > "::1 localhost"" >> /mnt/install.sh
+echo "nvim /etc/hosts > "123.0.1.1 ARCH"" >> /mnt/install.sh
+echo "passwd" >> /mnt/install.sh
+echo "useradd -mG wheel user1" >> /mnt/install.sh
+echo "passwd" >> /mnt/install.sh
+echo "efibootmgr --create --disk /dev/sdX --part Y --label "Arch Linux" --loader /vmlinuz-linux --unicode 'root=block_device_identifier rw initrd=\initramfs-linux.img'" >> install.sh
 
-arch-chroot /mnt && sh install.sh
-
-
+cd ~
+arch-chroot /mnt
 exit
 umount -R /mnt
-
 timedatectl set-ntp true
