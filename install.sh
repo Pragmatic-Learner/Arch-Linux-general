222c35b7ba22e936367bcbfec336b1d84cc0a909
Eternal Learner <nardevjuggoostudying@gmail.com>
2025-04-09 00:46:04 +0400

    11th commit

--- install.sh
+++ install.sh
@@ -1,110 +1,159 @@
-#setting up some variables
+#!/bin/bash
+#Setting Up time
 confirm="empty"
-
-#setting up time
-until [[ $confirm == "Y" ]] || [[ $confirm == "N" ]] || [[ -z $confirm ]]; do #Decide whether or not to change time
+until [[ $confirm == "Y" ]] || [[ $confirm == "N" ]] || [[ -z $confirm ]];#Decide whether to change timezone or not
 	clear && timedatectl status
 	echo -e "List of commands:\n\tY = Change timezone\n\tN = Do not change timezone"
-	read -p "Change timezone? [ default = Y ] " confirm && confirm=${confirm^^}
-	timezone="invalid"
+	read -p "Change timezone? [ default = N ] " confirm && confirm=${confirm^^}
 done
-while true; do #Change time until time is confirmed
-	Region="" && City=""
-
-	#Prompt
-	echo "Entering timezones... [ Format: Region/City ]"
+unset confirm 
+while true; do								#Keep Asking for Region And City input until proper matches are found
+	clear && echo "Entering timezones... [ Format: Region/City ]"
 	echo "Note :"
 	echo -e "\tFIRST LETTER of [[ Region ]] and [[ City ]] should be UPPERCASE."
 	echo -e "\tREST SHOULD BE LOWERCASE"
 	echo "Here is the available Regions And City"
-	cd /usr/share/zoneinfo && ls && echo "DISPLAYING REGIONS"
 
-	#Input
-	read -p "Enter timezone [ Region ] : " Region
+	cd /usr/share/zoneinfo && ls -Ad */ && echo -e "\n\nDISPLAYING REGIONS"
+	read -p "Enter timezone [ Region ] : " Region			#Region Input
+
 	if [ -d "/usr/share/zoneinfo/$Region" ]; then
-		cd /usr/share/zoneinfo/$Region && ls
-		read -p "Enter timezone [ City ] : " City
+		cd /usr/share/zoneinfo/$Region && ls -Ad */
+		read -p "Enter timezone [ City ] : " City		#City Input
+
 		if [ -f "/usr/share/zoneinfo/$Region/$City" ]; then
 			timedatectl set-timezone $Region/$City
+			break						#break
 		else
 			echo "ERROR!! CITY DOES NOT EXIST"
-			continue
 		fi
 	elif [ -f "/usr/share/zoneinfo/%Region" ]; then
 		timedatectl set-timezone $Region
+		break							#break
 	else
 		echo "ERROR!!  REGION DOES NOT EXIST"
-		continue
 	fi
-	break
+	unset Region && unset City
 done
-timedatectl set-ntp true
+timedatectl set-ntp true i&& unset Region && unset City
 
 #setting up disk partitions
-clear && lsblk && fdisk -l
+disk="notadrive"
+until [ -d "/sys/block/$disk" ]; do					#Repeat input of drive name until valid one is input
+	clear && lsblk && fdisk -l
+	read -p "Enter name of disk to format : " disk
+done
+echo "Disk found, beginning formatting..."
+
+until [[ $lgptdos == "gpt" ]] || [[ $lgptdos == "dos" ]]; do		#Repeat input of partition table until either gpt or dos is input
+	read -p "Enter label ( gpt / dos ) :: " lgptdos
+done
+
 echo "Add one of these suffix' to the end of the partition size: K, M, G, T, P"
-read -p "Enter size of EFI boot partition (default = 512MiB)	:: " efi
-read -p "Enter size of SWAP partition (default = None)		:: " swap
-read -p "Enter size of ROOT partition (default = ALL)		:: " root
+
+until [[ $efi =~ ^[0-9]+(K|M|G|T|P)$ ]] || [[ -z $efi ]]; do		#Repeat input of efi size until valid one is input
+	read -p "Enter size of EFI boot partition (default = 512MiB)	:: " efi
+done
+until [[ $swap =~ ^[0-9]+(K|M|G|T|P)$ ]] || [[ -z $swap ]]; do		#Repeat input of swap size until valid one is input
+	read -p "Enter size of SWAP partition (default = None)		:: " swap
+done
+until [[ $root =~ ^[0-9]+(K|M|G|T|P)$ ]] || [[ -z $root ]]; do		#Repeat input of root size until valid one is input
+	read -p "Enter size of ROOT partition (default = ALL)		:: " root
+done
+#Must add ability to choose root filesystem
+
 #setting up partition and filesystems
-if [[ -z $efi ]]; then
-	efi="512MiB"
-fi
-if [[ -z $root ]]; then
-	root="+"
-fi
+[[ -z $efi ]] && efi="512MiB"
+[[ -z $root ]] && root="+"
 if [[ -z $swap ]]; then
-	echo -e "label:gpt\n size=$efi, type=U\n size=$root, type=L" | sfdisk /dev/sda
+	echo -e "label:$lgptdos\n size=$efi, type=U\n size=$root, type=L" | sfdisk /dev/sda
+	
+	mkfs.fat -F 32 /dev/sda1						#Setting up filesystems
+	mkfs.ext4 /dev/sda2
+
+	mount /dev/sda2 /mnt							#Mounting partitions
+	mount --mkdir /dev/sda1 /mnt/boot
 else
-	echo -e "label:gpt\n size=$efi, type=U\n size=$swap, type=S\n size=$root, type=L" | sfdisk /dev/sda
+	echo -e "label:$lgptdos\n size=$efi, type=U\n size=$swap, type=S\n size=$root, type=L" | sfdisk /dev/sda
+
+	mkfs.fat -F 32 /dev/sda1						#Setting up filesystems
+	mkswap /dev/sda2
+	mkfs.ext4 /dev/sda3
+
+	mount /dev/sda3 /mnt							#Mounting partitions
+	mount --mkdir /dev/sda1 /mnt/boot
+	swapon /dev/sda2
 fi
+unset efi && unset swap && unset root
 
-#echo -e "g\nn\n\n\n$(efi)\nn\n\n\n$(swap)\nn\n\n\n$(root)\nt\n1\n1\nt\n2\n19\nw\n" | fdisk /dev/sda
+#installing packages			replace intel-ucode with amd-ucode if using amd cpu alright
+pacstrap -K /mnt base linux linux-firmware efibootmgr ffmpeg sudo reflector base-devel man git neovim networkmanager
+#pacstrap -K /mnt base linux linux-firmware base-devel intel-ucode git neovim efibootmgr grub networkmanager mesa xf86-video-intel vulkan-intel  libva-mesa-driver mesa-vdpau sof-firmware pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber kitty reflector openssh man sudo gimp
+#virtualbox-guest-utils
+#pcmanfm ffmpeg mpv
+#cutefish just to see how it looks
 
-#setting up filesystem
-mkfs.fat -F 32 /dev/sda1
-mkswap /dev/sda2
-mkfs.ext4 /dev/sda3
+genfstab -U /mnt >> /mnt/etc/fstab						#generate automatic mount points
 
-#mounting partitions
-mount /dev/sda3 /mnt
-mount --mkdir /dev/sda1 /mnt/boot
-swapon /dev/sda2
+cat <<END > /mnt/install.sh
+#!/bin/bash
+disk=$disk
+ln -sf /usr/share/zoneinfo/Time/Zone /etc/localtine
+hwclock --systohc
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "en_GB.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo "LANG=en_GB.UTF-8 UTF-8" >> /etc/locale.conf
+echo "KEYMAP=us" >> /etc/vconsole.conf
+echo "ARCH" >> /etc/hostname
+echo "123.0.0.1 localhost" >> /etc/hosts
+echo "::1 localhost" >> /etc/hosts
+echo "123.0.1.1 ARCH" >> /etc/hosts
 
-#installing packages
-#replace intel-ucode with amd-ucode if using amd cpu alright
-pacstrap -K /mnt base linux linux-firmware base-devel intel-ucode git neovim efibootmgr grub networkmanager mesa xf86-video-intel vulkan-intel  libva-mesa-driver mesa-vdpau sof-firmware pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber kitty reflector openssh man sudo gimp virtualbox-guest-utils cutefish pcmanfm ffmpeg mpv
+echo "Enter ROOT PASSWORD : "
+passwd
+read -p "Enter name for main Profile (THIS PROFILE WILL BE GRANTED WHEEL PRIVILEDGES, basicly admin) : " wUser
+useradd -mG wheel \$wuser
+echo "Enter password for main Profile :: "
+passwd
+while true; do
+	read -p "ADD ANOTHER USER ( Y/n ) ? " confirm && confirm=\${confirm^^}
+	if [[ \$confirm == "Y" ]]; then
+		read -p "Enter name of new user :: " nuser
+		if getent passwd \$nuser > /dev/null; then
+			echo "User already exist"
+			sleep 0.7
+		else
+			useradd -mG wheel \$nuser
+			echp -e "User \$nuser created\nEnter password for user \$nuser"
+			passwd \$nuser
+			clear
+		fi
+	elif [[ \$confirm == "N" ]]; then
+		echo "No more users to be added"
+		break
+	else
+		clear && echo "Invalid input"
+	fi
+done
+blkid /dev/"$disk"1
+echo ""
+read -p "WRITE THE PARTUUID exactly as is, into the input request :: " partuuid
+efibootmgr --create --disk /dev/$disk --part 1 --label "Arch Linux" --loader /vmlinuz-linux --unicode "root=PARTUUID=$partuuid rw initrd=\initramfs-linux.img"
+exit
+END
+unset disk && clear
+echo "sh install.sh" | arch-chroot /mnt
 
-#generate automatic mount points
-genfstab -U /mnt >> /mnt/etc/fstab
-
-echo "ln -sf /usr/share/zoneinfo/Time/Zone /etc/localtine" >> /mnt/install.sh
-echo "hwclock --systohc" >> /mnt/install.sh
-echo "echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen" >> /mnt/install.sh
-echo "echo "en_GB.UTF-8 UTF-8" >> /etc/locale.gen" >> /mnt/install.sh
-echo "locale-gen" >> /mnt/install.sh
-echo "echo "LANG=en_GB.UTF-8 UTF-8" >> /etc/locale.conf" >> /mnt/install.sh
-echo "echo "KEYMAP=us" >> /etc/vconsole.conf" >> /mnt/install.sh
-echo "echo "ARCH" >> /etc/hostname" >> /mnt/install.sh
-echo "echo "123.0.0.1 localhost" >> /etc/hosts" >> /mnt/install.sh
-echo "echo "::1 localhost" >> /etc/hosts" >> /mnt/install.sh
-echo "echo "123.0.1.1 ARCH" >> /etc/hosts" >> /mnt/install.sh
-echo "passwd" >> /mnt/install.sh
-echo "useradd -mG wheel user1" >> /mnt/install.sh
-echo "passwd" >> /mnt/install.sh
-echo "efibootmgr --create --disk /dev/sda --part 1 --label "Arch Linux" --loader /vmlinuz-linux --unicode 'root=block_device_identifier rw initrd=\initramfs-linux.img'" >> install.sh
-echo "exit" >> /mnt/install.sh
-
-echo "install.sh" | arch-chroot /mnt
-
+read -p "Checkpoint : " check
 umount -R /mnt
 timedatectl set-ntp true
-#systemctl enable NetworkManager
-#systemctl enable vboxservice.service
+systemctl enable NetworkManager
+systemctl enable vboxservice.service
 #systemctl start sddm.service
-#sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 
-#yay -Y --gendb
-#yay -Syu --devel
-#yay -Y --devel --save
-#yay brave-bin
+yay -Y --gendb
+yay -Syu --devel
+yay -Y --devel --save
+yay brave-bin
